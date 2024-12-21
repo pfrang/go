5bcf2996755e7819815dbee1e1ce2821447ca45b
Peder Frang <peder.frang@gmail.com>
2024-12-21 23:26:24 +0100

    Continue projecft

--- projects/mynew/src/main.go
+++ projects/mynew/src/main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	// create an API endpoint
 
-	server := "http://localhost:8080"
-	fmt.Println(server)
+	// resp, err := http.Get("http://localhost:8080")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	// Start the HTTP server
+
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
 
 }
