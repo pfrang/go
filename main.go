c3f38a8ca2fc6ec9e5a3e5b084c67aa471eab418
Peder Frang <peder.frang@gmail.com>
2024-12-23 09:38:40 +0100

    Continue projecft

--- main.go
+++ main.go
@@ -5,6 +5,18 @@ import (
 	"math"
 )
 
+func calculateDiscountRate(messagesSent int) float64 {
+	// ?
+
+	if messagesSent > 2500 {
+		return 0.2
+	} else if messagesSent > 1000 {
+		return 0.1
+	} else {
+		return 0
+	}
+}
+
 func main() {
 	var x, y int = 3, 4
 	var f = math.Sqrt(float64(x*x + y*y))
--- projects/mynew/src/main.go
+++ projects/mynew/src/main.go
@@ -5,6 +5,26 @@ import (
 	"net/http"
 )
 
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Request received")
+	h.ServeHTTP(w, r)
+}
+
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Middleware executed")
+
+		// some inf oiabout client
+
+		clientInfo := r.Header.Get("User-Agent")
+		fmt.Println(clientInfo)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	// create an API endpoint
 
@@ -16,7 +36,9 @@ func main() {
 
 	// Start the HTTP server
 
+	// handler := &MyHandler{}
+
 	fmt.Println("Server is running on port 8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", middleware(http.DefaultServeMux))
 
 }
