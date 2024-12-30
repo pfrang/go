63291fd74f8ce94e3bbf8e001ed909e86edc336e
Peder Frang <peder.frang@gmail.com>
2024-12-31 00:57:40 +0100

    Start refactor Handlers

--- projects/mynew/src/api/main.go
+++ projects/mynew/src/api/main.go
@@ -4,27 +4,28 @@ import (
 	"net/http"
 )
 
+type Handler func(http.ResponseWriter, *http.Request)
+
+// func listEndpoints(w http.ResponseWriter, r *http.Request) {
+// 	utils.HandleResponse(w, r, Endpoints)
+// }
+
 // Define a struct with some fields
 type Endpoint struct {
 	Path        string `json:"path"`
 	Description string `json:"description"`
+	Handler     Handler
 }
 
 // Define a slice of endpoints
 var Endpoints = []Endpoint{
-	{Path: "/", Description: "Root endpoint"},
-	{Path: "/create-user", Description: "Create a new user"},
-	{Path: "/list-endpoints", Description: "List all available endpoints"},
+	{Path: "/", Description: "Root endpoint", Handler: Root},
+	{Path: "/create-user", Description: "Create a new user", Handler: CreateUser},
+	// {Path: "/list-endpoints", Description: "List all available endpoints", Handler: listEndpoints},
 }
 
 func RegisterEndpoints() {
-
 	for _, endpoint := range Endpoints {
-		switch endpoint.Path {
-		case "/":
-			http.HandleFunc(endpoint.Path, Root)
-		case "/create-user":
-			http.HandleFunc(endpoint.Path, CreateUser)
-		}
+		http.HandleFunc(endpoint.Path, endpoint.Handler)
 	}
 }
